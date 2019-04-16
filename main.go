c11f581a75a0d45def1a3f37b8953fa4097cc855
Ricky Pike <arcanericky@gmail.com>
2019-04-16 11:38:58 -0500

    Get body with custom Transport and RoundTripper

--- main.go
+++ main.go
@@ -1,12 +1,56 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 
 	apiclient "github.com/arcanericky/petstoreclient/client"
 	"github.com/arcanericky/petstoreclient/client/pet"
 	"github.com/arcanericky/petstoreclient/models"
+	httptransport "github.com/go-openapi/runtime/client"
+	"github.com/go-openapi/strfmt"
 )
+
+type customRoundTripper struct {
+	defaultRoundTripper http.RoundTripper
+	body                string
+}
+
+func (crt *customRoundTripper) RoundTrip(r *http.Request) (*http.Response, error) {
+	resp, err := crt.defaultRoundTripper.RoundTrip(r)
+	if err != nil {
+		return nil, err
+	}
+
+	responseBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	crt.body = string(responseBytes)
+
+	err = resp.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	resp.Body = ioutil.NopCloser(bytes.NewReader(responseBytes))
+
+	return resp, nil
+}
+
+func newCustomTransport(crt *customRoundTripper) *httptransport.Runtime {
+	transport := httptransport.New(apiclient.DefaultTransportConfig().Host,
+		apiclient.DefaultTransportConfig().BasePath,
+		apiclient.DefaultTransportConfig().Schemes)
+
+	crt.defaultRoundTripper = transport.Transport
+	transport.Transport = crt
+
+	return transport
+}
 
 func addPet(id int64, petName string, urls []string) {
 	newPet := models.Pet{ID: id, Name: &petName, PhotoUrls: urls}
@@ -46,9 +90,33 @@ func getPet(id int64) {
 	}
 }
 
+// My attempt to retrieve the response body by using a custom
+// RoundTripper inside a custom Transport. The custom Roundtripper
+// reads and stores the response, then replaces the original response
+// with a new Reader. It works, but it is correct?
+func getPetWithBody(id int64) {
+	getPetParams := pet.NewGetPetByIDParams()
+	getPetParams.PetID = id
+
+	crt := new(customRoundTripper)
+	client := apiclient.New(newCustomTransport(crt), strfmt.Default)
+
+	fmt.Println("Fetching pet")
+	if authOk, err := client.Pet.GetPetByID(getPetParams, nil); err == nil {
+		fmt.Println(authOk)
+		fmt.Printf("ID: %d\nName: %s\nPhotoUrls: %s\n",
+			authOk.Payload.ID,
+			*authOk.Payload.Name,
+			authOk.Payload.PhotoUrls)
+		fmt.Println("Body:", crt.body)
+	} else {
+		fmt.Println(err)
+	}
+}
+
 func main() {
 	const petID = 13371337
 
 	addPet(petID, "geek", []string{"https://en.wikipedia.org/wiki/Geek"})
-	getPet(petID)
+	getPetWithBody(petID)
 }
